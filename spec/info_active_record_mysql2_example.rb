8d07f95683341eed00a37838600f369db24ee4ac
kaspernj <k@spernj.org>
2015-12-30 19:21:16 +0100

    Added example config files

---
+++ spec/info_active_record_mysql2_example.rb
@@ -0,0 +1,35 @@
+class Baza::InfoActiveRecordMysql2
+  attr_reader :db
+
+  def self.connection
+    require "active_record"
+
+    @conn_pool ||= ::ActiveRecord::Base.establish_connection(
+      adapter: "mysql2",
+      host: "localhost",
+      database: "baza-test",
+      username: "baza-test",
+      password: "password"
+    )
+    @conn ||= @conn_pool.connection
+
+    {pool: @conn_pool, conn: @conn}
+  end
+
+  def initialize(args = {})
+    data = Baza::InfoActiveRecordMysql2.connection
+
+    @db = Baza::Db.new({
+      type: :active_record,
+      conn: data[:conn]
+    }.merge(args))
+  end
+
+  def before
+    @db.tables.list(&:drop)
+  end
+
+  def after
+    @db.close
+  end
+end
---
+++ spec/info_active_record_mysql_example.rb
@@ -0,0 +1,37 @@
+class Baza::InfoActiveRecordMysql
+  attr_reader :db
+
+  def self.connection
+    require "active_record"
+    require "pg"
+
+    @conn_pool ||= ::ActiveRecord::Base.establish_connection(
+      adapter: "mysql",
+      host: "localhost",
+      database: "baza-test",
+      username: "baza-test",
+      password: "password"
+    )
+    @conn ||= @conn_pool.connection
+
+    {pool: @conn_pool, conn: @conn}
+  end
+
+  def initialize(args = {})
+    @data = Baza::InfoActiveRecordMysql.connection
+    @data.fetch(:conn).reconnect!
+
+    @db = Baza::Db.new({
+      type: :active_record,
+      conn: @data.fetch(:conn)
+    }.merge(args))
+  end
+
+  def before
+    @db.tables.list(&:drop)
+  end
+
+  def after
+    @db.close
+  end
+end
