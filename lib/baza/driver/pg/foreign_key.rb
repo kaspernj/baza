1bbadc37869448296c9aea9c4a793ebd1b56f05f
kaspernj <kaspernj@gmail.com>
2017-01-22 15:51:18 +0100

    Removed unnecessary and unused instance variable

--- lib/baza/driver/pg/foreign_key.rb
+++ lib/baza/driver/pg/foreign_key.rb
@@ -1,9 +1,11 @@
 class Baza::Driver::Pg::ForeignKey < Baza::ForeignKey
   def initialize(args)
     @db = args.fetch(:db)
-    @data = args.fetch(:data)
-    @column_name = @data.fetch(:column_name)
-    @name = @data.fetch(:constraint_name)
-    @table_name = @data.fetch(:table_name)
+
+    data = args.fetch(:data)
+
+    @column_name = data.fetch(:column_name)
+    @name = data.fetch(:constraint_name)
+    @table_name = data.fetch(:table_name)
   end
 end
