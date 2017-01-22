e7ee3493578677da5e73a9d93deeba248bb91aef
kaspernj <kaspernj@gmail.com>
2017-01-22 15:52:20 +0100

    Cache column result and use underscore for cached method results

--- lib/baza/foreign_key.rb
+++ lib/baza/foreign_key.rb
@@ -4,11 +4,11 @@ class Baza::ForeignKey
   attr_reader :column_name, :db, :name, :table_name
 
   def column
-    table.column(column_name)
+    @_column ||= table.column(column_name)
   end
 
   def table
-    @table ||= db.tables[table_name]
+    @_table ||= db.tables[table_name]
   end
 
   def to_param
