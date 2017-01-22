a0adff5a5f1c5cb872cc6a2e7c03acc6270eab97
kaspernj <kaspernj@gmail.com>
2017-01-22 15:50:57 +0100

    Removed debug output

--- lib/baza/driver/pg/foreign_key.rb
+++ lib/baza/driver/pg/foreign_key.rb
@@ -1,7 +1,5 @@
 class Baza::Driver::Pg::ForeignKey < Baza::ForeignKey
   def initialize(args)
-    puts "Args: #{args}"
-
     @db = args.fetch(:db)
     @data = args.fetch(:data)
     @column_name = @data.fetch(:column_name)
