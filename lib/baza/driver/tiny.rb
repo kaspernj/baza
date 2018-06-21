2da370ff0ea7c291be1f3ec6d65d7846334c4994
kjabtion <kj@abtion.com>
2018-06-21 14:41:02 +0000

    Fixed syntax error

--- lib/baza/driver/tiny.rb
+++ lib/baza/driver/tiny.rb
@@ -14,8 +14,6 @@ class Baza::Driver::Tiny < Baza::BaseSqlDriver
     @sep_val = SEPARATOR_VALUE
     @sep_index = SEPARATOR_INDEX
 
-  def initialize(db)
-    super
     @client = TinyTds::Client.new(username: db.opts.fetch(:user), password: db.opts.fetch(:pass), host: db.opts.fetch(:host))
   end
 
