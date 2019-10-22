c876738d00c54462f1d391191ab8b0d4d67f7ef8
Kasper Johansen <kaspernj@gmail.com>
2019-10-22 17:32:31 +0200

    Pg driver: pg_stat_statements native table (#66)

---
+++ spec/drivers/pg/table_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe Baza::Driver::Pg::Table do
+  let(:constant) do
+    const_name = "InfoPg"
+    require StringCases.camel_to_snake(const_name)
+    raise "Constant was not defined: '#{const_name}'." unless Baza.const_defined?(const_name)
+    Baza.const_get(const_name)
+  end
+  let(:driver) { constant.new }
+  let(:db) { driver.db }
+
+  describe "#native?" do
+    it "returns true if the table is 'pg_stat_statements'-table" do
+      test_table = Baza::Driver::Pg::Table.new(driver: driver, data: {table_name: "pg_stat_statements"})
+
+      expect(test_table.native?).to eq true
+    end
+  end
+end
