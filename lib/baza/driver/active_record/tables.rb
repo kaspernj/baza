723d6d3dd626569c95dd7cec543c46a23964a823
kaspernj <k@spernj.org>
2023-08-01 10:20:55 +0000

    Fixed offences

--- lib/baza/driver/active_record/tables.rb
+++ lib/baza/driver/active_record/tables.rb
@@ -6,7 +6,7 @@ class Baza::Driver::ActiveRecord::Tables
     @proxy_to = ::Baza::Driver.const_get(StringCases.snake_to_camel(@args.fetch(:db).driver.driver_type)).const_get(:Tables).new(**@args)
   end
 
-  def method_missing(name, *args, **kwargs, &blk)
-    @proxy_to.__send__(name, *args, **kwargs, &blk)
+  def method_missing(...)
+    @proxy_to.__send__(...)
   end
 end
--- lib/baza/driver/active_record/users.rb
+++ lib/baza/driver/active_record/users.rb
@@ -6,7 +6,7 @@ class Baza::Driver::ActiveRecord::Users
     @proxy_to = ::Baza::Driver.const_get(StringCases.snake_to_camel(@args.fetch(:db).driver.driver_type)).const_get(:Users).new(@args)
   end
 
-  def method_missing(name, *args, &blk)
-    @proxy_to.__send__(name, *args, &blk)
+  def method_missing(...)
+    @proxy_to.__send__(...)
   end
 end
